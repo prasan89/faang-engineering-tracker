a824092cd21c4d3705e532e93cd46d3c8b515446
prasan89 <prasan.mail89@gmail.com>
2026-09-06 04:31:38 +0530

    Seed DSA problem bank

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+MERGE INTO dsa_problem (id, title, url, difficulty, pattern) KEY(id) VALUES
+('two-sum','Two Sum','https://leetcode.com/problems/two-sum/','Easy','Hash Map'),
+('valid-anagram','Valid Anagram','https://leetcode.com/problems/valid-anagram/','Easy','Hash Map'),
+('group-anagrams','Group Anagrams','https://leetcode.com/problems/group-anagrams/','Medium','Hash Map'),
+('longest-substring','Longest Substring Without Repeating Characters','https://leetcode.com/problems/longest-substring-without-repeating-characters/','Medium','Sliding Window'),
+('best-time-stock','Best Time to Buy and Sell Stock','https://leetcode.com/problems/best-time-to-buy-and-sell-stock/','Easy','Two Pointers'),
+('valid-parentheses','Valid Parentheses','https://leetcode.com/problems/valid-parentheses/','Easy','Stack'),
+('binary-search','Binary Search','https://leetcode.com/problems/binary-search/','Easy','Binary Search'),
+('reverse-linked-list','Reverse Linked List','https://leetcode.com/problems/reverse-linked-list/','Easy','Linked List'),
+('maximum-depth-tree','Maximum Depth of Binary Tree','https://leetcode.com/problems/maximum-depth-of-binary-tree/','Easy','Trees'),
+('number-of-islands','Number of Islands','https://leetcode.com/problems/number-of-islands/','Medium','Graphs'),
+('climbing-stairs','Climbing Stairs','https://leetcode.com/problems/climbing-stairs/','Easy','Dynamic Programming'),
+('coin-change','Coin Change','https://leetcode.com/problems/coin-change/','Medium','Dynamic Programming');
